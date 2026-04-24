c25598fb6365d7e477d9c487e01e692cbd592b4e
Seop2 <57839278+Seop2@users.noreply.github.com>
2026-04-24 14:18:48 +0900

    Title: 1부터 특정 수까지의 합, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260424/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+let sum = 0;
+for(let i=1; i<=n; i++){
+    sum+= i;
+}
+console.log(Math.floor(sum / 10));
