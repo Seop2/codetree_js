c8f30b10d198f04379c6d18e8e32404e5ecff6a4
Seop2 <57839278+Seop2@users.noreply.github.com>
2026-04-24 14:30:19 +0900

    Title: 짝수이면서 합이 5의 배수인 수, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260424/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+let answer ="";
+
+function isEven(n){
+    if(n% 2=== 0)return true;
+    else false;
+}
+
+function isFive(n){
+    let x = Math.floor(n / 10);
+    let y = n % 10;
+    
+    let sum = x + y;
+    if(sum % 5 ===0) answer = "Yes";
+    else answer = "No";
+}
+
+if(isEven(n)){
+    isFive(n);
+    console.log(answer);
+}
+
