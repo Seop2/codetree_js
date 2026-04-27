966fe161c88e2091b83254c2fc259a8e36be69c1
Seop2 <57839278+Seop2@users.noreply.github.com>
2026-04-27 17:55:30 +0900

    Title: 사칙연산 함수, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260427/사칙연산 함수/quadratic-operations-function.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+let [a, o, c] = input[0].split(" ");
+a = Number(a);
+c = Number(c);
+
+const cal = ['+', '-', '/', "*"]
+
+if(o === cal[0]){
+    console.log(`${a} ${o} ${c} = ${a + c}`)
+}else if(o === cal[1]){
+    console.log(`${a} ${o} ${c} = ${a - c}`)
+}
+else if(o === cal[2]){
+    console.log(`${a} ${o} ${c} = ${a / c}`)
+}
+else if(o === cal[3]){
+    console.log(`${a} ${o} ${c} = ${a * c}`)
+}else{
+    console.log("False");
+}
+    
