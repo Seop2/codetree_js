2c21b07529d5d34bcfe7db41a2b9e923c5b3fd96
Seop2 <57839278+Seop2@users.noreply.github.com>
2026-04-28 16:23:28 +0900

    Title: 두 정수 값 교환하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260428/두 정수 값 교환하기/to-exchange-two-integer-values.js
+++ 260428/두 정수 값 교환하기/to-exchange-two-integer-values.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
-const [n, m] = input[0].split(" ").map(Number);
+let [n, m] = input[0].split(" ").map(Number);
 
 function swap(n, m){
     [n, m] = [m, n];
     return [n, m];
 }
-swap(n, m);
+[n,m] = swap(n, m);
 
 
 console.log(n, m);
