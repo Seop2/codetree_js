fa8992c90ab04e6b7dbdd021c8330d616cb49c9d
Seop2 <57839278+Seop2@users.noreply.github.com>
2026-05-06 10:00:22 +0900

    [Passed] 특정 구간의 합 (76ms, 10MB)

--- trail2/특정 구간의 합/sum-of-specific-intervals.js
+++ trail2/특정 구간의 합/sum-of-specific-intervals.js
@@ -1,25 +1,25 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 const [n, m] = input[0].split(" ").map(Number);
-const A = input[1].split(" ").map(Number); // temp
+const A = input[1].split(" ").map(Number);
 const queries = input.slice(2).map(line => line.split(" ").map(Number));
 
-const arr = Array(n+1);
+const arr = Array(n + 1);
 arr[0] = 0;
 
-for(let i = 1; i<=n; i++){
+for (let i = 1; i <= n; i++) {
     arr[i] = A[i - 1];
 }
 
-function getAnswer(a1, a2){
+function getAnswer(a1, a2) {
     let returnValue = 0;
-    for(let i = a1; i<= a2; i++){
+    for (let i = a1; i <= a2; i++) {
         returnValue += arr[i];
     }
     return returnValue;
 }
 
-for(let i = 0; i< queries.length; i++){
-    let [a1 , a2] = queries[i];
+for (let i = 0; i < queries.length; i++) {
+    let [a1, a2] = queries[i];
     console.log(getAnswer(a1, a2));
 }
