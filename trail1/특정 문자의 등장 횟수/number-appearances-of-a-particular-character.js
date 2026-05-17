d6dab41867466895d71ec81ef0cc793a51504114
Seop2 <57839278+Seop2@users.noreply.github.com>
2026-05-17 13:52:36 +0900

    [Passed] 특정 문자의 등장 횟수 (68ms, 10MB)

--- trail1/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.js
+++ trail1/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.js
@@ -1,13 +1,10 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim();
-
-let len = input.length;
-let cnt1 = 0;
-let cnt2 = 0;
-
-for(let i= 0; i<len - 1; i++){
-    if(input[i] === 'e' && input[i+1] ==='e')cnt1++;
-    if(input[i]==='e' && input[i+1] === 'b')cnt2++;
+let count1 = 0;
+let count2 = 0;
+for(let i=0; i<input.length; i++){
+    if(input[i] === 'e' && input[i+1] === 'e')count1++;
+    if(input[i]==='e'&& input[i+1]==='b')count2++;
 }
 
-console.log(cnt1, cnt2);
+console.log(count1, count2);
